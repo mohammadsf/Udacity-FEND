0b21fbf331fe6549ca225200881093ef104f56a6
mjyousse <mjyousse@gmail.com>
2018-07-05 16:18:14 -0700

    Quiz 8

---
+++ Part 2 - Web Programming with JavaScript/Lesson 2 - Data Types and Variables/Section 08 - Quiz - Converting Temperatures (2-2)/Quiz.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Converting Tempatures (2-2)
+ *
+ * Use the Celsius-to-Fahrenheit formula to set the fahrenheit varible:
+ *
+ *     F = C x 1.8 + 32
+ *
+ * Log the fahrenheit variable to the console.
+ * Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12°C.
+ */
+
+ var celsius= 12;
+ var fahrenheit= celsius * 1.8 + 32;
+ consol.logo(fahrenheit);
