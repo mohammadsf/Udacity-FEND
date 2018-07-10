16b746bf7c95d057b159b592ab18aec2053af051
mjyousse <mjyousse@gmail.com>
2018-07-09 19:31:22 -0700

    Added all quizes from Section 2

---
+++ Part 2 - Web Programming with JavaScript/Lesson 2 - Data Types and Variables/Section 13 - Quiz - String Equality for All (2-4)/Quiz.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ * Fix the right side expression so it evaluates to true.
+
+"ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal"
+ */
+
+
+
+// fix the right side of the expression
+var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
---
+++ Part 2 - Web Programming with JavaScript/Lesson 2 - Data Types and Variables/Section 21 - Quiz - What's my Name (2-9)/Quiz.js
@@ -0,0 +1,7 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ * Create a variable called fullName and assign it your full name as a string.
+ */
+
+// your code goes here
+var fullName = 'Mohammad Youssef';
