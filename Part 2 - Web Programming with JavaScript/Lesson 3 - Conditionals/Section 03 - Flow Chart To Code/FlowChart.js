7fa07ebb1d78245d19aed7e75f17dbf8b04febb2
mjyousse <mjyousse@gmail.com>
2018-07-10 17:25:01 -0700

    FlowChart Code Added.

---
+++ Part 2 - Web Programming with JavaScript/Lesson 3 - Conditionals/Section 03 - Flow Chart To Code/FlowChart.js
@@ -0,0 +1,11 @@
+/*
+This is a given flowchart and how to code it.
+*/
+
+var price = 15.00; // price of hammer
+var money = 20.00; // How much I have on me
+if (money >= price) {
+    console.log("buy the hammer");
+} else {
+    console.log("don't buy the hammer");
+}
