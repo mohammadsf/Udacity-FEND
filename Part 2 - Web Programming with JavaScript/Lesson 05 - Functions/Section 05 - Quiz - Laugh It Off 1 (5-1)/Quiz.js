6f55bbbd649df53154a65ff08949dc6bd22d12c7
mjyousse <mjyousse@gmail.com>
2018-07-26 16:16:13 -0700

    Added all quizes

---
+++ Part 2 - Web Programming with JavaScript/Lesson 05 - Functions/Section 05 - Quiz - Laugh It Off 1 (5-1)/Quiz.js
@@ -0,0 +1,9 @@
+/*
+Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+Programming Quiz: Laugh it Off 1 (5-1)
+
+*/
+function laugh(){
+    return "hahahahahahahahahaha!";
+}
+console.log(laugh());
