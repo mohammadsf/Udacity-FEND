69d0023621a54fb7efa770fff9ff95e249f7cdd3
mjyousse <mjyousse@gmail.com>
2018-08-25 12:50:47 -0700

    Code modified

--- Part 2 - Web Programming with JavaScript/Lesson 03 - Conditionals/Section 07 - Quiz - Musical Groups (3-3)/Quiz.js
+++ Part 2 - Web Programming with JavaScript/Lesson 03 - Conditionals/Section 07 - Quiz - Musical Groups (3-3)/Quiz.js
@@ -21,22 +21,20 @@ If musicians equals -1, then not a group should be printed to the console.
  */
 
 // change the value of `musicians` to test your conditional statements
-var musicians = 1;
+var musicians = 5;
 
 // your code goes here
 
-    if (musicians <= 0) {
-        console.log('not a group');
-    } else if (musicians ===1 ) {
-        console.log('Solo');
-    } else if (musicians === 2) {
-        console.log('duet');
-    } else if (musicians === 3) {
-        console.log('trio');
-    } else if (musicians === 4) {
-        console.log('quartet');
-    } else if (musicians >= 4) {
-        console.log('This is a large group')''
-    }
-
-
+if (musicians <=0) {
+    console.log("not a group");
+} else if (musicians === 1) {
+    console.log("solo");
+} else if (musicians === 2) {
+    console.log("duet");
+} else if (musicians === 3) {
+    console.log("trio");
+} else if (musicians === 4) {
+    console.log("quartet");
+} else if (musicians > 4) {
+    console.log("this is a large group");
+}
