4cbf733360aea542ca5fb95d856a9dce5165294d
mjyousse <mjyousse@gmail.com>
2018-09-04 17:32:59 -0700

    Updated Quiz Solution

---
+++ Part 2 - Web Programming with JavaScript/Lesson 05 - Functions/Section 22 - Quiz - Inline (5-6)/Quiz.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// call the emotions function here and pass in an
+// inline function expression
+
+emotions("happy", function laugh(num) {
+
+    var silly = "";
+    for (x = 1; x <= num; x++) {
+        silly += "ha";
+        }
+       return silly + "!"; 
+});
